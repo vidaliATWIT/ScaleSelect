34dc7123a25968a6ce36ac4865d45111f9da0340
Ubuntu <ubuntu@ip-172-31-18-185.us-east-2.compute.internal>
2024-07-18 01:44:26 +0000

    Reconfigured code to use apache2 within an ec2 Ubuntu container

--- scaleproject/scaleproject/settings.py
+++ scaleproject/scaleproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-&r161)+_#e-x3etrh6t4p5%47_kqr-l%puwg019untxpsuwylj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -59,7 +60,9 @@ ROOT_URLCONF = 'scaleproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+		os.path.join(BASE_DIR, 'build'),
+	],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/ubuntu/home/ScaleSelect/scaleproject/build/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
